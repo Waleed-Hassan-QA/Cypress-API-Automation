73e6cb959a59c00a31cb6c3053ad3c8f46b68c2a
Waleed Hassan <waleedhassan909@gmail.com>
2025-06-24 15:04:40 +0500

    get user api

--- cypress/e2e/getUsers.cy.js
+++ cypress/e2e/getUsers.cy.js
@@ -1,7 +1,24 @@
- <reference types="Cypress" />
+/// <reference types="Cypress" />
 
 describe('API Automation',()=>{
 
+it('Get Users', ()=>{
+
+    cy.request({
+
+        method:'GET',
+        url:'https://gorest.co.in/public/v2/users',
+        headers:{
+            Authorization:'Bearer eb906c43a292135c4a1cd99ec58b54af482fb068720f95e7e5ee3e64292e0610'
+        }
+    }).then((response)=>{
+        //cy.log(JSON.stringify(response))
+        expect(response.status).to.equal(200)
+    })
+
+
+
+})
 
 
 })
